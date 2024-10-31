6bc1e2cde2dbf164a90048b8708e7afdfd59a8b7
Théo Rousseaux <theo.rousseaux@hotmail.com>
2024-10-31 17:18:24 +0100

    avancement video test + UI pour une image

--- frontend/pose_estimation.py
+++ frontend/pose_estimation.py
@@ -7,31 +7,32 @@ sys.path.append(os.getcwd())
 from src.pose_estimation.estimator import model
 from frontend.utils import save_uploaded_file
 
-if "file_path" not in st.session_state:
-    st.session_state["file_path"] = None
 
 ############ Main ############
 
 st.title("Pose Estimation")
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+col1, col2 = st.columns(2)
+
+with col1:
+    uploaded_file = st.file_uploader(
+        "Choose an file...", type=["jpg", "jpeg", "png", "mp4", "avi", "mov"]
+    )
 
 if uploaded_file is not None:
-    st.session_state["file_path"] = save_uploaded_file(uploaded_file)
-
-if st.session_state["file_path"] is not None:
-
-    col1, col2 = st.columns(2)
     with col1:
-        st.image(st.session_state["file_path"], use_column_width=True)
-
         apply_button = st.button("Apply Pose Estimation")
 
     if apply_button:
-        with col2:
-            results = model(source=st.session_state["file_path"])
+        file_path = save_uploaded_file(uploaded_file)
+        # The file is an image
+        if uploaded_file.name.endswith(("jpg", "jpeg", "png")):
+            results = model(source=file_path)
             img = results[0].plot()
-            # Conversion de BGR à RGB
             img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            with col2:
+                st.image(img_rgb, use_column_width=True)
 
-            st.image(img_rgb, use_column_width=True)
+        # The file is a video
+        else:
+            results = model(source=st.session_state["file_path"])
